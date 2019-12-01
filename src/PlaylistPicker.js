e515ff45dccbddbf6cfaa1ae78d7f5571252775b
Daniel Margulis <damargulis@macbook-pro.lan>
2019-12-01 15:58:51 -0500

    playlist skel

--- src/PlaylistPicker.js
+++ src/PlaylistPicker.js
@@ -31,12 +31,26 @@ export default class PlaylistPicker extends React.Component {
   }
 
   render() {
+    // todo: make auto different -- top heard, unheard, move errors to herei
+    // instead of a filter, etc.
     return (
       <div className="main">
-        <WrappedGrid
-          items={this.state.playlists}
-          cellRenderer={this.cellRenderer.bind(this)}
-        />
+        <div style={{position: 'absolute', height: '100%', width: '100%'}}>
+          <div style={{height: '100%', width: '50%'}}>
+            Manual
+            <WrappedGrid
+              items={this.state.playlists}
+              cellRenderer={this.cellRenderer.bind(this)}
+            />
+          </div>
+          <div style={{height: '100%', position: 'absolute', top: 0, right: 0, width: '50%'}}>
+            <span style={{width: '100%', textAlign: 'center'}}>Auto</span>
+            <WrappedGrid
+              items={this.state.playlists}
+              cellRenderer={this.cellRenderer.bind(this)}
+            />
+          </div>
+        </div>
       </div>
     );
   }
