fe8d7c7515bd4e813d82968a90fe600d9e403f3d
Daniel Margulis <damargulis@macbook-pro.lan>
2020-01-19 23:06:55 -0500

    lint

--- public/electron.js
+++ public/electron.js
@@ -5,14 +5,14 @@ const {
   Menu,
   MenuItem,
   ipcMain,
-  shell
-} = require('electron');
+  shell,
+} = require("electron");
 
 // shouldn't need package for this ... figure out better way
-const isDev = require('electron-is-dev');
+const isDev = require("electron-is-dev");
 
-const path = require('path');
-const defaultMenu = require('electron-default-menu');
+const path = require("path");
+const defaultMenu = require("electron-default-menu");
 
 let mainWindow = null;
 let extensionWindow = null;
@@ -23,47 +23,47 @@ let extensionWindow = null;
 let extEvt = null;
 let extensionEvt = null;
 
-ipcMain.on('goToArtist', (evt, data) => {
+ipcMain.on("goToArtist", (evt, data) => {
   maximize();
-  evt.reply('toArtist', data);
+  evt.reply("toArtist", data);
 });
 
-ipcMain.on('goToSong', (evt, data) => {
+ipcMain.on("goToSong", (evt, data) => {
   maximize();
-  evt.reply('toSong', data);
+  evt.reply("toSong", data);
 });
 
-ipcMain.on('goToAlbum', (evt, data) => {
+ipcMain.on("goToAlbum", (evt, data) => {
   maximize();
-  evt.reply('toAlbum', data);
+  evt.reply("toAlbum", data);
 });
 
-ipcMain.on('minimize', (evt) => {
+ipcMain.on("minimize", (evt) => {
   //mainWindow.setSize(300, 100);
   mainWindow.setSize(240, 120);
-  evt.reply('minimize-reply');
+  evt.reply("minimize-reply");
 });
 
-ipcMain.on('maximize', (evt) => {
+ipcMain.on("maximize", (evt) => {
   maximize();
-  evt.reply('maximize-reply');
+  evt.reply("maximize-reply");
 });
 
 /** Maximize the main window. */
 function maximize() {
   mainWindow.setSize(1430, 800);
 }
-ipcMain.on('extension-ready', (evt) => {
+ipcMain.on("extension-ready", (evt) => {
   extEvt = evt;
 });
 
-ipcMain.on('extension-monitor-ready', (evt) => {
+ipcMain.on("extension-monitor-ready", (evt) => {
   extensionEvt = evt;
 });
 
 const messageQueue = [];
 
-ipcMain.on('extension-update', (evt, arg) => {
+ipcMain.on("extension-update", (evt, arg) => {
   messageQueue.push(arg);
   if (extensionEvt) {
     while (messageQueue.length) {
@@ -73,7 +73,7 @@ ipcMain.on('extension-update', (evt, arg) => {
   }
 });
 
-ipcMain.on('extension-close', () => {
+ipcMain.on("extension-close", () => {
   if (extensionWindow) {
     extensionWindow.close();
   }
@@ -81,7 +81,7 @@ ipcMain.on('extension-close', () => {
 
 /** Forward event to reset the library from main to the App. */
 function resetLibrary() {
-  extEvt.reply('reset-library');
+  extEvt.reply("reset-library");
 }
 
 /**
@@ -89,74 +89,72 @@ function resetLibrary() {
  * @param {string} type The type of extension to run.
  */
 function runExtension(type) {
-  extEvt.reply('run-extension', type);
+  extEvt.reply("run-extension", type);
   extensionEvt = null;
   extensionWindow = new BrowserWindow({
-    width: 500,
     height: 250,
     webPreferences: {
       nodeIntegration: true,
       webSecutiry: false,
     },
+    width: 500,
   });
   extensionWindow.loadURL(
-    `file://${path.join(__dirname, './extension_monitor.html')}`);
+    `file://${path.join(__dirname, "./extension_monitor.html")}`);
 }
 
 /** Creates the main window. */
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1430,
-    height: 800,
     frame: false,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false,
     },
+    width: 1430,
   });
   mainWindow.loadURL(isDev
-    ? 'http://localhost:3000'
-    : `file://${path.join(__dirname, './index.html')}`);
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "./index.html")}`);
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.on("closed", () => mainWindow = null);
   const menu = defaultMenu(app, shell);
   menu.push(new MenuItem({
     label: "Library",
-    submenu: [{label: "Reset from itunes", click: () => resetLibrary()}]
+    submenu: [{label: "Reset from itunes", click: () => resetLibrary()}],
   }));
   menu.push(new MenuItem({
     label: "Extensions",
-    submenu: [{label: "Wikipedia", click: () => runExtension('wikipedia')}]
+    submenu: [{label: "Wikipedia", click: () => runExtension("wikipedia")}],
   }));
   Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
   // TODO: media keys dont work ... :(
-  globalShortcut.register('CommandOrControl+0', () => {
-    extEvt.reply('nextTrack');
+  globalShortcut.register("CommandOrControl+0", () => {
+    extEvt.reply("nextTrack");
   });
-  globalShortcut.register('CommandOrControl+9', () => {
-    extEvt.reply('playTrack');
+  globalShortcut.register("CommandOrControl+9", () => {
+    extEvt.reply("playTrack");
   });
-  globalShortcut.register('CommandOrControl+8', () => {
-    extEvt.reply('prevTrack');
+  globalShortcut.register("CommandOrControl+8", () => {
+    extEvt.reply("prevTrack");
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
 });
-
-
