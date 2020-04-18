12ee82a65a8139602ef3b7b06b3bfc6c2c894afd
Daniel Margulis <damargulis@MacBook-Pro.local>
2020-04-18 18:01:16 -0400

    .

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -1,6 +1,5 @@
 import currentlyPlaying from './currentlyPlaying';
 import library from './library';
-// import newTracks from './newTracks';
 import {combineReducers} from 'redux';
 
 export default combineReducers({currentlyPlaying, library});
