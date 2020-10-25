4f23cee9eaae4575d1691db4c893de2f6cc73a15
Daniel Margulis <damargulis@MacBook-Pro-1403.local>
2020-10-25 15:16:04 -0400

    add artist

--- src/ArtistPicker.tsx
+++ src/ArtistPicker.tsx
@@ -1,13 +1,20 @@
 import {Artist} from './redux/actionTypes';
+import ArtistEditor from './ArtistEditor';
 import ArtistInfo from './ArtistInfo';
 import React from 'react';
+import Modal from 'react-modal';
 import SearchBar from './SearchBar';
+import shortid from 'shortid';
 import WrappedGrid from './WrappedGrid';
+
+// see: http://reactcommunity.org/react-modal/accessibility/#app-element
+Modal.setAppElement('#root');
 
 interface ArtistPickerState {
   search: string;
   sortedArtists: Artist[];
   withErrors: boolean;
+  addingArtist: boolean;
 }
 
 interface ArtistPickerProps {
@@ -24,6 +31,7 @@ export default class ArtistPicker extends React.Component<ArtistPickerProps, Art
       search: '',
       sortedArtists: [],
       withErrors: false,
+      addingArtist: false,
     };
 
     this.numCols = 0;
@@ -47,14 +55,33 @@ export default class ArtistPicker extends React.Component<ArtistPickerProps, Art
     const items = this.state.sortedArtists;
     return (
       <div className="main">
+        <Modal isOpen={this.state.addingArtist} onRequestClose={this.closeAddArtist.bind(this)}>
+          <ArtistEditor exit={this.closeAddArtist.bind(this)} artist={{
+            id: shortid.generate(),
+            name: '',
+            albumIds: [],
+            errors: [],
+            genreIds: [],
+            trackIds: [],
+          }} />
+        </Modal>
         <button onClick={() => this.withErrors()}>Show Wiki Status</button>
         <SearchBar onSearch={(search) => this.onSearch(search)} />
+        <button onClick={() => this.addArtist()}>+</button>
         <WrappedGrid
           cellRenderer={this.cellRenderer.bind(this)}
           numItems={items.length}
         />
       </div>
     );
+  }
+
+  private addArtist(): void {
+    this.setState({addingArtist: true});
+  }
+
+  private closeAddArtist(): void {
+    this.setState({addingArtist: false});
   }
 
   private sortArtists(artists: Artist[]): Artist[] {
